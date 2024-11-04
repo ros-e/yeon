179f0ac16411099ec93f5fadc9cc31af5d1f57f6
Rose <snowydg@proton.me>
2024-11-03 21:20:52 -0600

    data

---
+++ src/model.py
@@ -0,0 +1,5 @@
+import torch
+
+class x0:
+    def __init__(self):
+        pass
--- src/train.py
+++ src/train.py
@@ -10,13 +10,22 @@ import argparse
 import sys
 from pathlib import Path
 import time
+from dataset import data_loader
 
 class Trainer:
     def __init__(self):
+    #stolen from gpt2
+        n_vocab = 0,
+        n_ctx = 1024,
+        n_embd = 768,
+        n_head = 12,
+        n_layer = 12,
         self.parse_args()
         self.train()
     def parse_args(self):
         parser = argparse.ArgumentParser()
+        parser.add_argument('--batch-size', type=int, default=12, required=False, help='Number of samples processed in one iteration during training')
+        parser.add_argument('--device', type=str, choices=['cpu', 'cuda'], default='cpu', help='Specify the device to use for training: "cpu" or "cuda".')
         parser.add_argument('--log-dir', type=str, required=True, help='Directory to store training logs')
         parser.add_argument('--checkpoint-dir', type=str, required=True, help='Directory to store model checkpoints')
         parser.add_argument('--data-dir', type=str, required=True, help='Directory containing training data')
@@ -28,11 +37,5 @@ class Trainer:
         self.args = parser.parse_args()
 
     def train(self):
-        if self.args.disable_progress_bar:
-            print("Progress bar is disabled.")
-        else:
-            print("Progress bar is enabled.")
-
-if __name__ == '__main__':
-    trainer = Trainer()
-    args = trainer.parse_args()
+        data_loader()
+            
